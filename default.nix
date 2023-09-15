bf84e5b5c01a87dc9221e12ec38a5403d90e3c0a
Răzvan Flavius Panda <razvan.panda@gmail.com>
2023-09-16 00:28:36 +0300

    #

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
 { pkgs ? import (fetchTarball "https://releases.nixos.org/nixpkgs/nixpkgs-23.11pre525954.46688f8eb5cd/nixexprs.tar.xz") { } }:
 
-pkgs.mkShell {
+pkgs.stdenv.mkDerivation rec {
+  name = "betterdiscord";
+#   version = "1.0.0";
+
+  src = ./.;
+
   buildInputs = [
     pkgs.nodejs_20
     (pkgs.nodePackages.pnpm.override { 
@@ -8,8 +13,13 @@ pkgs.mkShell {
     })
   ];
 
-  shellHook = ''
+  buildPhase = ''
     pnpm install
     pnpm build
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r ./dist/* $out/
   '';
 }
